89fa146be3449b54a373d6b844e50de303a2a13f
V Patel <viralpatel4@gmail.com>
2017-06-04 21:05:56 -0400

    First commit

---
+++ src/app/app.chatroom.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'chat-room',
+    templateUrl: './app.chatroom.html',
+    styleUrls: ['./app.chatroom.css']
+})
+export class AppChatroom {
+    title = 'Chat Room';
+}
